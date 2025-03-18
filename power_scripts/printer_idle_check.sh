ef3136059616e4c6390312b4ea9bafcbe793cd50
tigattack <10629864+tigattack@users.noreply.github.com>
2025-03-18 22:19:55 +0000

    fix:(printer_idle_check): only output idle check start when log level >=info

--- power_scripts/printer_idle_check.sh
+++ power_scripts/printer_idle_check.sh
@@ -9,7 +9,9 @@ sleep $PRINTER_IDLE_CHECK_INTERVAL
 
 # Check loop
 while true; do
-    echo "Checking printer idle status..."
+    if [[ -z "$PRINTER_IDLE_LOGLEVEL" || "INFO DEBUG TRACE" =~ $PRINTER_IDLE_LOGLEVEL ]]; then
+        echo "Checking printer idle status..."
+    fi
     /opt/power_scripts/printer_idle.py
     sleep $PRINTER_IDLE_CHECK_INTERVAL
 done
